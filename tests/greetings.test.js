26904ff0597356337b01c2beef8940a1597438b5
Bazsi <bazsi@Emeses-MacBook.local>
2026-01-19 07:51:18 +0100

    Creates tests for having a birthday, and not having a birthday.

--- tests/greetings.test.js
+++ tests/greetings.test.js
@@ -1,15 +1,16 @@
 const fs = require("fs");
-const MessageSender = require ("../src/MessageSender.js").default;
+const MessageSender = require("../src/MessageSender.js").default;
+const RealDate = Date;
 
 jest.mock("fs", () => ({
-    readFile: jest.fn()
+  readFile: jest.fn(),
 }));
 
-const mockDate = mockedDate => {
-    jest 
-    .spyOn(global, "Date")
-    .mockImplementation(() => new Date(mockedDate));
-}
+const mockDate = (mockedDate) => {
+  jest.spyOn(global, "Date").mockImplementation(() => {
+    return new RealDate(mockedDate);
+  });
+};  
 
 afterEach(() => {
   jest.restoreAllMocks();
@@ -17,13 +18,68 @@ afterEach(() => {
 
 
 
-describe("Birthday-Greetings", ()=> {
-test("calls fs.readFile when MessageSender is created", () => {
-  fs.readFile.mockImplementation(() => {});
 
-  new MessageSender();
+describe("Birthday-Greetings", () => {
+  test("calls fs.readFile when MessageSender is created", () => {
+    fs.readFile.mockImplementation(() => {});
+    new MessageSender();
+    expect(fs.readFile).toHaveBeenCalledTimes(1);
+  });
+
+
+
+
+  test("sendMessages when there are NO birthdays", (done) => {
+    mockDate("2024-01-01");
+
+    fs.readFile.mockImplementation((path, encoding, callback) => {
+      callback(
+        null,
+        JSON.stringify([
+          {
+            first_name: "John",
+            last_name: "Doe",
+            dob: "2000-01-16",
+            email: "john.doe@foobar.com",
+          },
+        ]),
+      );
+    });
+
+    const sender = new MessageSender();
+
+    setImmediate(() => {
+      expect(() => sender.sendMessages()).not.toThrow();
+      done();
+    });
+  });
+
+
+
   
-  expect(fs.readFile).toHaveBeenCalledTimes(1);
-});
+  test("sendMessages when there are birthdays", (done) => {
+    mockDate("2024-05-05");
 
-})
+    fs.readFile.mockImplementation((path, encoding, callback) => {
+      callback(
+        null,
+        JSON.stringify([
+          {
+            first_name: "Kovacs",
+            last_name: "Anna",
+            dob: "1999-05-05",
+            email: "anna.kovacs@foobar.com",
+          },
+        ]),
+      );
+    });
+
+    const sender = new MessageSender();
+
+    setImmediate(() => {
+      sender.sendMessages();
+      expect(sender.friends.length).toBe(1);
+      done();
+    });
+  });
+});
